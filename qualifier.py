84eeb31ae9d5a1d811467989a76b8c145c15cb86
Shubham Upreti <shubham.upreti@gmail.com>
2020-07-23 02:39:58 +0530

    Basic requirements done

--- qualifier.py
+++ qualifier.py
@@ -15,6 +15,8 @@ Important notes for submission:
 """
 import datetime
 import typing
+import collections
+import re
 
 
 class ArticleField:
@@ -28,4 +30,41 @@ class Article:
     """The `Article` class you need to write for the qualifier."""
 
     def __init__(self, title: str, author: str, publication_date: datetime.datetime, content: str):
-        pass
+        self.title = title
+        self.author = author
+        self.publication_date = publication_date
+        self.content = content
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} title={repr(self.title)} "\
+               f"author={repr(self.author)} "\
+               f"publication_date={repr(self.publication_date.isoformat())}>"
+
+    def __len__(self):
+        return len(self.content)
+
+    def short_introduction(self, n_characters: int):
+        """Returns a short description of the article"""
+        if self.content[n_characters-1] not in [' ', '\n']:
+
+            try:
+                n_characters = max(
+                    self.content.rindex(' ', 0, n_characters),
+                    self.content.rindex('\n', 0, n_characters)
+                    )
+            except ValueError:
+                if not self.content.rindex(' ', 0, n_characters):
+                    n_characters = self.content.rindex('\n', 0, n_characters)
+                else:
+                    n_characters = self.content.rindex(' ', 0, n_characters)
+
+            return self.content[:n_characters]
+
+        return self.content[:n_characters].rstrip()
+
+    def most_common_words(self, n_words: int):
+        """Returns freq of most common words as a dict"""
+        words = re.findall(r'[\w]+', self.content.lower())
+        wordfreq = collections.Counter(words).most_common(n_words)
+
+        return dict(wordfreq)
